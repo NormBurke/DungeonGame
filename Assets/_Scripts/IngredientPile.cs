5ac27504190088c302021e3bc8cc20f421045532
NormBurke <39559054+NormBurke@users.noreply.github.com>
2018-07-04 21:29:22 +1000

    vrclasswed

---
+++ Assets/_Scripts/IngredientPile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class IngredientPile : MonoBehaviour {
+
+	//public Object IngredientPrefab;
+	public GameObject startingIngredient;
+
+	
+	// Use this for initialization
+	void Start () {
+		//GameObject NewObj = Instantiate(startingIngredient, startingIngredient.transform.position, startingIngredient.transform.rotation) as GameObject;
+		//startingIngredient.GetComponent<Renderer>().enabled=false;
+		//NewObj.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SpawnIngredient(object sender, InteractableObjectEventArgs e) {
+		//((VRTK_InteractableObject)sender).gameObject.GetComponent<Renderer>().enabled = true;
+		GameObject NewObj = Instantiate(startingIngredient, startingIngredient.transform.position, startingIngredient.transform.rotation) as GameObject;
+		NewObj.transform.parent = startingIngredient.transform.parent;
+		NewObj.transform.localScale = startingIngredient.transform.localScale;
+		//NewObj.GetComponent<Renderer>().enabled=false;
+	}
+}
